41c33373c7d8dc267a90d87ae308247f08f934e4
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-11-01 16:15:27 -0300

    funcionamento basico de midlewares

--- StartUp.cs
+++ StartUp.cs
@@ -13,8 +13,26 @@ namespace Hosting
     }
 
     public void Configure(IApplicationBuilder app){
+      
+      string ordem = string.Empty;
+
       app.Use( async (context, next) => {
-        await context.Response.WriteAsync("Trabalhando com class StartUp : " + _configuration["Application"]);
+
+        ordem += "1";
+        await next.Invoke(); //chama o proximo midleware
+
+        ordem += "4";
+        await context.Response.WriteAsync($"Ordem: {ordem}");
+        //await context.Response.WriteAsync("Trabalhando com class StartUp : " + _configuration["Application"]);
+      });
+
+      app.Use( async (context, next) => {
+
+        ordem += "2";
+        await next.Invoke(); //chama o proximo midleware
+        
+        ordem += "3";        
+        //await context.Response.WriteAsync("Welcome");        
       });
     }
 
